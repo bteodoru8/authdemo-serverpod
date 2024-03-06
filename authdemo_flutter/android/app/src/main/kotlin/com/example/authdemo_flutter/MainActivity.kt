dcb1b3ea3f431815de6fda136accd2454fbe8ea5
Bogdan Teodoru <a.teodoru8@gmail.com>
2024-03-06 16:21:01 +0100

    full app

---
+++ authdemo_flutter/android/app/src/main/kotlin/com/example/authdemo_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.authdemo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ authdemo_flutter/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:authdemo_client/authdemo_client.dart';
+import 'package:authdemo_flutter/src/pages/account_page.dart';
+import 'package:authdemo_flutter/src/pages/sign_in_page.dart';
+import 'package:authdemo_flutter/src/serverpod_client.dart';
+import 'package:flutter/material.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+
+// Sets up a singleton client object that can be used to talk to the server from
+// anywhere in our app. The client is generated from your server code.
+// The client is set up to connect to a Serverpod running on a local server on
+// the default port. You will need to modify this to connect to staging or
+// production servers.
+var client = Client('http://$localhost:8080/')
+  ..connectivityMonitor = FlutterConnectivityMonitor();
+
+void main() async {
+  // Need to call this as SessionManager is using Flutter bindings before runApp
+  // is called.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeServerpodClient();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Serverpod Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Serverpod Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Make sure that we rebuild the page if signed in status changes.
+    sessionManager.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: sessionManager.isSignedIn ? const AccountPage() : const SignInPage(),
+    );
+  }
+}
